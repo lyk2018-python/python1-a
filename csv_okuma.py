4ed52fc7ee591ca8900821f6f4258a8fb4e50b67
mustafa <mustafa@genomize.com>
2018-07-29 20:00:52 +0300

    csv okuma baslangic

--- csv_okuma.py
+++ csv_okuma.py
@@ -1,5 +1,13 @@
 import csv
 
+sayilar = []
 with open("test.csv") as dosya:
-    data
+    rows = csv.reader(dosya,delimiter=";",quotechar='"')
+    for row in list(rows)[1:]:
+        if row[3].isnumeric():
+            sayi = int(row[3])
+            sayilar.append(sayi)
+    print("max",max(sayilar))
+    print("min",min(sayilar))
+
 
